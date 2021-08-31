03e6dc9b4d868ad76cdbef41842c79d53d782c9c
Talha shamim <Talhashamim001@gmail.com>
2021-08-31 21:45:35 +0530

    mail added

--- controller/controller.js
+++ controller/controller.js
@@ -64,7 +64,7 @@ export const rateInc = async (req,res) => {
     try{
         await rate.save();
         res.status(200).json({
-            success : true;
+            success : true,
         })
     }
     catch(err){
